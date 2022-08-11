9e357b03e411141384e481e56ce0577f4edfdedb
ColdMagi <alexsky99@skiff.com>
2022-08-11 21:23:33 +0300

    heading and boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Heading, Input } from "@chakra-ui/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function Start() {
+  return (
+    <>
+      <Input placeholder="https://tjournal.ru/" size="lg" />
+    </>
+  );
+}
 
 function App() {
-  return <ChakraProvider></ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <BrowserRouter>
+        <header>
+          <Heading as="h1">TJScope</Heading>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Start />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  );
 }
 
 export default App;
