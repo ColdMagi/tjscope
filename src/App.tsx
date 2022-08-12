a8b8f20703b5dc8140bd270fd19a5526dd3aa060
ColdMagi <alexsky99@skiff.com>
2022-08-12 19:54:12 +0300

    wrap into container, start page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import { ChakraProvider, Heading, Input } from "@chakra-ui/react";
+import {
+  Button,
+  ChakraProvider,
+  Container,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function Start() {
   return (
-    <>
-      <Input placeholder="https://tjournal.ru/" size="lg" />
-    </>
+    <InputGroup mt="4" size="lg">
+      <Input pr="6.2rem" placeholder="https://tjournal.ru/" size="lg" />
+      <InputRightElement width="6rem">
+        <Button h="2.5rem" size="sm">
+          Scooope
+        </Button>
+      </InputRightElement>
+    </InputGroup>
   );
 }
 
@@ -13,13 +26,23 @@ function App() {
   return (
     <ChakraProvider>
       <BrowserRouter>
-        <header>
-          <Heading as="h1">TJScope</Heading>
-        </header>
+        <Container my={4}>
+          <header className="">
+            <Heading
+              as="h1"
+              bgGradient="linear(to-l, blue.500, yellow.200)"
+              bgClip="text"
+              fontSize="6xl"
+              fontWeight="extrabold"
+            >
+              TJScope
+            </Heading>
+          </header>
 
-        <Routes>
-          <Route path="/" element={<Start />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Start />} />
+          </Routes>
+        </Container>
       </BrowserRouter>
     </ChakraProvider>
   );
