1fd1111956de4fec72b2557f19c4672319e80b05
ColdMagi <alexsky99@skiff.com>
2022-08-12 22:16:13 +0300

    user agent locale defition

--- src/hooks/useUserAgent.ts
+++ src/hooks/useUserAgent.ts
@@ -1,6 +1,6 @@
 function useUserAgent(app = "tj-app") {
   return {
-    "User-agent": `${app}/2.2.0; ${navigator.userAgent}; ${Intl.Locale}; ${window.innerHeight}x${window.innerWidth}`,
+    "User-agent": `${app}/2.2.0; ${navigator.userAgent}; ${navigator.language}; ${window.innerHeight}x${window.innerWidth}`,
   };
 }
 
