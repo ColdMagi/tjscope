483a4fe0251edbcf1992c6eaf96c3d2a87cfa8b2
ColdMagi <alexsky99@skiff.com>
2022-08-16 22:03:00 +0300

    refactor: add user

---
+++ src/components/User.tsx
@@ -0,0 +1,35 @@
+import { HStack, Avatar, Text } from "@chakra-ui/react";
+import { PropsWithChildren } from "react";
+
+interface UserProps {
+  name: string;
+  avatar_url: string;
+  id: number | string;
+  size?: "sm" | "md";
+}
+
+function User({
+  name,
+  avatar_url,
+  id,
+  children,
+  size = "md",
+}: PropsWithChildren<UserProps>) {
+  return (
+    <HStack as="a" href={`https://tjournal.ru/u/${id}`} target="_blank">
+      <Avatar size={size} name={name} src={avatar_url} />
+      <Text
+        fontSize={size === "md" ? "18px" : "14px"}
+        as="b"
+        maxW={{ base: "150px", md: "180px", lg: "200px" }}
+        textOverflow={"ellipsis"}
+        overflow="hidden"
+      >
+        {name}
+      </Text>
+      {children}
+    </HStack>
+  );
+}
+
+export default User;
