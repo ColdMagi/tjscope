4ca931ea1fa7ae29ddb4035dbe6e549021e7e4d7
ColdMagi <alexsky99@skiff.com>
2022-08-17 23:15:46 +0300

    version inc

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
             TJScope
           </Heading>
           <chakra.span pl="2" color="gray.400">
-            v0.3.16-alpha
+            v0.3.17-alpha
           </chakra.span>
         </chakra.header>
 
