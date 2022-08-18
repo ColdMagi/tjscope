7630df35f3cc7d0fe44c80a810d8600748a7e353
ColdMagi <alexsky99@skiff.com>
2022-08-18 23:36:22 +0300

    sortable TotalTable

--- src/components/scope/TotalTable.tsx
+++ src/components/scope/TotalTable.tsx
@@ -1,5 +1,4 @@
 import {
-  TableContainer,
   Table,
   TableCaption,
   Thead,
@@ -7,43 +6,88 @@ import {
   Th,
   Tbody,
   Td,
+  chakra,
 } from "@chakra-ui/react";
 import User from "components/User";
 import type { Osnova } from "types/osnova";
+import { useTable, useSortBy, Column } from "react-table";
+import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
+import { useMemo } from "react";
+
+const columns: Column<Osnova.Likers.Liker>[] = [
+  {
+    Header: "Пользователь",
+    accessor: (d) => d,
+    Cell: ({
+      value: { name, avatar_url, id },
+    }: {
+      value: Osnova.Likers.Liker;
+    }) => <User name={name} avatar_url={avatar_url} id={id} />,
+  },
+  {
+    Header: "+",
+    accessor: "plus",
+    isNumeric: true,
+  },
+  {
+    Header: "-",
+    accessor: "minus",
+    isNumeric: true,
+  },
+  {
+    Header: "Всего",
+    accessor: "total",
+    isNumeric: true,
+  },
+];
 
 function TotalTable({ likers }: { likers: Osnova.Likers.Likers }) {
+  const data = useMemo(() => [...Object.values(likers)], [likers]);
+  // https://codesandbox.io/s/fxx0v?file=/src/App.js:968-998
+  const { getTableProps, getTableBodyProps, headerGroups, prepareRow, row } =
+    useTable({ columns, data }, useSortBy);
+
   return (
-    <TableContainer>
-      <Table variant="simple" size={{ base: "sm" }}>
-        <TableCaption>Статистика оценок по пользователям</TableCaption>
-        <Thead>
-          <Tr>
-            <Th>Пользователь</Th>
-            <Th isNumeric color="green.300">
-              +
-            </Th>
-            <Th isNumeric color="red.300">
-              -
-            </Th>
-            <Th isNumeric>Всего</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {[...Object.entries(likers)]
-            .sort(([_, a], [_0, b]) => b.plus + b.minus - (a.plus + a.minus))
-            .map(([id, { name, avatar_url, minus, plus }]) => (
-              <Tr key={id}>
-                <Td>
-                  <User name={name} avatar_url={avatar_url} id={id} />
-                </Td>
-                <Td>{plus}</Td>
-                <Td>{minus}</Td>
-                <Td>{minus + plus}</Td>
-              </Tr>
+    <Table size="sm" {...getTableProps()}>
+      <TableCaption>Статистика оценок по пользователям</TableCaption>
+      <Thead>
+        {headerGroups.map((headerGroup) => (
+          <Tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <Th
+                {...column.getHeaderProps(column.getSortByToggleProps())}
+                isNumeric={column.isNumeric}
+              >
+                {column.render("Header")}
+                <chakra.span pl="4">
+                  {column.isSorted ? (
+                    column.isSortedDesc ? (
+                      <TriangleDownIcon aria-label="sorted descending" />
+                    ) : (
+                      <TriangleUpIcon aria-label="sorted ascending" />
+                    )
+                  ) : null}
+                </chakra.span>
+              </Th>
             ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+          </Tr>
+        ))}
+      </Thead>
+      <Tbody {...getTableBodyProps()}>
+        {row.map((row) => {
+          prepareRow(row);
+          return (
+            <Tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <Td {...cell.getCellProps()} isNumeric={cell.column.isNumeric}>
+                  {cell.render("Cell")}
+                </Td>
+              ))}
+            </Tr>
+          );
+        })}
+      </Tbody>
+    </Table>
   );
 }
 
